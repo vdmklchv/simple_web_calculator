51cfd60a8b267990c1f815869b85bcbad5ca10de
Vadim Colcev <vadimkolchev@gmail.com>
2020-04-26 12:41:46 +0300

    Release v0.1

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ const inputArea = document.getElementsByTagName('input')[0];
 inputArea.value = "0";
 
 keypadDiv.addEventListener('click', (event) => {
+    const lastChar = inputArea.value.charAt(inputArea.value.length - 1);
     switch (event.target.value) {
         case 'reset':
             inputArea.value = "0";
@@ -17,35 +18,66 @@ keypadDiv.addEventListener('click', (event) => {
             break;
         case 'plus':
             if (inputArea.value === '0'){
-                inputArea.value = '';
+                inputArea.value = '+';
             }
-            if (inputArea.value.charAt(inputArea.length -1) != '+') {
+            else if (lastChar === '*' || lastChar === '-' || lastChar === '/' || lastChar === '+')    {
+                let newStr = inputArea.value.slice(0, -1);
+                inputArea.value = newStr + '+';
+            }
+            else {
                 inputArea.value += '+';
             }
             break;
         case 'minus':
             if (inputArea.value === '0'){
-                inputArea.value = '';
+                inputArea.value = '-';
             }
-            if (inputArea.value.charAt(inputArea.length -1) != '-') {
+            else if (lastChar === '*' || lastChar === '-' || lastChar === '/' || lastChar === '+')    {
+                let newStr = inputArea.value.slice(0, -1);
+                inputArea.value = newStr + '-';
+            }
+            else {
                 inputArea.value += '-';
             }
             break;
         case 'multiply':
             if (inputArea.value === '0'){
-                inputArea.value = '';
+                inputArea.value = '*';
             }
-            if (inputArea.value.charAt(inputArea.length -1) != '*') {
+            else if (lastChar === '*' || lastChar === '-' || lastChar === '/' || lastChar === '+')    {
+                let newStr = inputArea.value.slice(0, -1);
+                inputArea.value = newStr + '*';
+            }
+            else {
                 inputArea.value += '*';
             }
             break;
         case 'divide':
             if (inputArea.value === '0'){
-                inputArea.value = '';
+                inputArea.value = '/';
             }
-            if (inputArea.value.charAt(inputArea.length -1) != '/') {
+            else if (lastChar === '*' || lastChar === '-' || lastChar === '/' || lastChar === '+')    {
+                let newStr = inputArea.value.slice(0, -1);
+                inputArea.value = newStr + '/';
+            }
+            else {
                 inputArea.value += '/';
-            } 
-            break;              
+            }
+            break;
+        case "dot":
+             inputArea.value += '.';
+            break;
+        
+        case 'equals':
+            const totalString = inputArea.value;
+            inputArea.value = eval(totalString);
+
+        default:
+            if (+(event.target.value) >= 0 && +(event.target.value) <= 9)   {
+                if (inputArea.value === '0')    {
+                    inputArea.value = '';
+                }
+            inputArea.value += event.target.value;
+    }
     }
 })
